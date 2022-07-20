f53cb60fc2cee1217b9f8942df427bef309ef968
France Fortin <France_fortin1@hotmail.com>
2022-07-20 09:39:27 -0700

    added masonry setup

--- scripts/project.js
+++ scripts/project.js
@@ -1,1 +1,12 @@
-set_cookie("shopping_cart_items", items);
+$('.grid').masonry({
+    // options
+    itemSelector: '.grid-item',
+    columnWidth: 200
+  });
+
+/* set_cookie("shopping_cart_items", items); // to store data for shopping cart */
+
+/*let catalog_container = document.getElementById("catalog"); // assuming your target is <div class='row' id='catalog'>
+jQuery(catalog_container).imagesLoaded( function() {
+var msnry = new Masonry(catalog_container); // this initializes the masonry container AFTER the product images are loaded
+}); THIS GOES IMMEDIATELY AFTER YOU RENDER YOUR CATALOG*/

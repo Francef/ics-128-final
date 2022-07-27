37e1652c38b847ef3bb47968cc0e0db8b260a598
France Fortin <France_fortin1@hotmail.com>
2022-07-27 11:45:28 -0700

    Added "Add to Cart" functionality. Started creating the shopping cart

--- scripts/project.js
+++ scripts/project.js
@@ -14,31 +14,52 @@ class Catalog {
     }
     render_catalog() {
         for (let i = 0; i < this.product_data.length; i++) {
+            let price = this.product_data[i].price;
+            price = parseFloat(price).toFixed(2);
             let card = `<div class="col-sm-6 col-lg-4 mb-4"><div class="card">
-					<svg class="bd-placeholder-img card-img-top" width="100%" height="200"
-						xmlns="` + this.product_data[i].image + `" role="img" aria-label="Placeholder: Image cap"
-						preserveAspectRatio="xMidYMid slice" focusable="false">
-						<title>Placeholder</title>
-						<rect width="100%" height="100%" fill="#868e96" /><text x="50%" y="50%" fill="#dee2e6"
-							dy=".3em">Image cap</text>
-					</svg>
+					<img src="` + this.product_data[i].image +`" alt="` + this.product_data[i].title + `">
 					<div class="card-body">
 						<h5 class="card-title">`+ this.product_data[i].title + `</h5>
-                        <p class="card-text">` + this.product_data[i].price + `</p>
+                        <p class="card-text">` + price + `</p>
 						<p class="card-text">` + this.product_data[i].description + `</p>
+                        <button class="addToCart" data-id="` + this.product_data[i].id + `">Add to Cart</button>
 					</div>
-				</div></div>`;
+				</div>
+                </div>`;
             document.querySelector(`#catalog`).innerHTML += card;
         }
         let catalog_container = document.getElementById("catalog"); // assuming your target is <div class='row' id='catalog'>
         jQuery(catalog_container).imagesLoaded(function () {
             var msnry = new Masonry(catalog_container); // this initializes the masonry container AFTER the product images are loaded
         });
+
+        this.add_event_handlers();
     }
+
+add_event_handlers() {
+
+ //let data = get_cookie("shopping_cart_items");
+
+ $(".addToCart").click(function() {
+    //get the product id that had its "add to cart" button clicked
+    var product_id = $(this).attr("data-id");
+    var cart_items = get_cookie("shopping_cart_items"); // get the data stored as a cookie
+
+    // initialize the cart items if it returns null
+    if (cart_items === null) {
+        cart_items = {};
+    }
+
+    // make sure object is defined
+    if (cart_items[product_id] === undefined) {
+        cart_items[product_id] = 0;
+    }
+
+    cart_items[product_id]++;
+    console.log(cart_items); //TAKE THIS OUT LATER!!!!!!!!!!!!!!!!!!!!!!!
+    $(this).prepend(`<span class="badge badge-light">` + cart_items[product_id] + `</span>`); // !!!!!!!!!!!!!!!THIS DOESN'T WORK GREAT :/ ADDS MULTIPLE NUMBERS
+    set_cookie("shopping_cart_items", cart_items); // setting the cart items back to the cookie storage
+});
 }
-
+}
 let catalog = new Catalog();
-
-
-
-/* set_cookie("shopping_cart_items", items); // to store data for shopping cart */
